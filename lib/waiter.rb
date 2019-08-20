ebcc9e2e777f361d50eecc9e05895984358c36b2
Shahwar Spall <shevy.spall@gmail.com>
2019-08-20 23:48:43 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,7 +16,7 @@ def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
 def meals
-  meals.all.select do |meal|
+  Meal.all.select do |meal|
   meal.waiter == self
 end
 end
